9f239a3f38d1ee6291eacb6a490421fdbf191c89
Kevin.R <kevin.rajendram@digitalseeder.com>
2023-05-26 12:25:21 +0200

    User CRUD ok

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
